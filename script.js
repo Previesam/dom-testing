41bb0bad66043320ebb29a73a731bf7444daee58
Previesam <samsmart.ass59@gmail.com>
2021-03-20 11:31:21 +0100

    Added tasks and implemented task creation form and storage in local storage for persistency

--- script.js
+++ script.js
@@ -2,14 +2,3 @@ var content = document.querySelector(".content");
 window.onload = () => {
   showDashboard();
 };
-
-var handleChange = (e) => {
-    console.log(e.target.checked)
-    if (e.target.checked) {
-        alert("checked");
-        return
-    }
-
-    alert("not checked")
-    
-}
--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,9 +1,4 @@
 var showTask = async (e) => {
-  var createTodo = (e) => {
-    e.preventDefault;
-    console.log(e.target.todoInput.value);
-    return false 
-  };
   var activeLink = document.querySelectorAll(".active");
   activeLink.forEach((el) => {
     el.classList.remove("active");
@@ -15,65 +10,87 @@ var showTask = async (e) => {
     if (xhr.readyState == 4) {
       content.innerHTML = xhr.response;
       // window.history.replaceState("Task", "Task", "/tasks");
+
+      if (!localStorage.getItem("tasks")) {
+        var tasks = [];
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+      }
+
+      var tasks = JSON.parse(localStorage.getItem("tasks"));
+
+      var taskContainer = document.querySelector(".task-container");
+
+      var todoForm = document.querySelector("#todoForm");
+
+      var checkbox
+
+      var updateTaskList = (task) => {
+        var taskItem = document.createElement("div");
+
+        taskItem.setAttribute("class", "task-header");
+
+        var taskMenuBtn = document.createElement("p");
+
+        taskMenuBtn.setAttribute("class", "menu-btn");
+
+        taskMenuBtn.innerHTML = `<span></span><span></span><span></span>`;
+
+        var textDiv = document.createElement("div");
+
+        textDiv.classList.add("input-field");
+
+        var newCheckbox = document.createElement("input");
+
+        newCheckbox.setAttribute("type", "checkbox");
+
+        newCheckbox.setAttribute("onchange", `updateStatus(event, ${task.id})`);
+
+        textDiv.innerHTML = task.details;
+
+        taskItem.appendChild(taskMenuBtn);
+
+        taskItem.appendChild(textDiv);
+
+        taskItem.appendChild(newCheckbox);
+
+        taskContainer.appendChild(taskItem);
+      };
+
+      var setInitialTaskList = () => {
+        tasks.forEach((task) => {
+          updateTaskList(task);
+        });
+      };
+
+      setInitialTaskList();
+
+      var createTask = (event) => {
+        event.preventDefault();
+        if (!event.target.todoInput.value) return;
+        var task = { id: 1, details: event.target.todoInput.value, done: false };
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        updateTaskList(task);
+        return false
+      };
+
+      todoForm.addEventListener("submit", createTask, false);
+
+      var handleChange = (e) => {
+        if(e.target.checked) {
+          console.log("Checked")
+        }
+        console.log("Unchecked")
+      }
+
     }
   };
-
   await xhr.open("GET", "tasks/index.html", true);
   xhr.responseText = "document";
   await xhr.send();
-
-  var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-
-  var taskContainer = document.querySelector(".task-container");
-
-  var updateTasks = () => {
-    tasks.forEach((task) => {
-      newTask(task);
-    });
-  };
-
-  var handleInput = (e) => {
-    if (e.target.value.length < 3) return;
-    console.log(e.target.value);
-  };
-
-  var newTask = (task) => {
-    var taskItem = document.createElement("div");
-
-    taskItem.setAttribute("class", "task-header");
-
-    var taskMenuBtn = document.createElement("p");
-
-    taskMenuBtn.setAttribute("class", "menu-btn");
-
-    taskMenuBtn.innerHTML = `<span></span><span></span><span></span>`;
-
-    var textDiv = document.createElement("div");
-
-    textDiv.classList.add("input-field");
-
-    var newCheckbox = document.createElement("input");
-
-    newCheckbox.setAttribute("type", "checkbox");
-
-    newCheckbox.setAttribute("onchange", `updateStatus(event, ${task.id})`);
-
-    textDiv.innerHTML = task.details;
-
-    taskItem.appendChild(taskMenuBtn);
-
-    taskItem.appendChild(textDiv);
-
-    taskItem.appendChild(newCheckbox);
-
-    taskContainer.appendChild(taskItem);
-  };
-
-  if (!tasks) {
-    return;
-  }
-  updateTasks();
 };
 
-document.querySelector("form").addEventListener("submit");
+var handleInput = (e) => {
+  if (e.target.value.length < 3) return;
+  console.log(e.target.value);
+};
